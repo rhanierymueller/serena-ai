21c96957b2465d4ef3aee57dfed2068668a98e6a
rhanierymueller <rhanimueller@hotmail.com>
2025-04-27 13:55:56 -0300

    fixes

--- server/src/routes/stripeWebhook.ts
+++ server/src/routes/stripeWebhook.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import type Stripe from "stripe";
 import { prisma } from "../lib/prisma.js";
-import stripe from "../lib/stripe.js";
+import {stripe} from "../lib/stripe.js";
 
 const router = express.Router();
 
